6e491e602bfa973139897626b90e86cdae258348
Caleb <reateguicevallosc@gmail.com>
2023-06-27 14:07:10 -0500

    Delete: Static Data

--- src/app/modules/schedules/schedules.component.ts
+++ src/app/modules/schedules/schedules.component.ts
@@ -45,50 +45,6 @@ export class SchedulesComponent {
 
       });
     });
-
-    this.ELEMENT_DATA = [
-      {
-        cuota: 1668.25,
-        name: "Cronograma 1".toUpperCase(),
-        tem: 0.32779,
-        saldoInicial: 90000,
-        seguroDesgravamen: 0.028,
-        van: 23973.30,
-        tir: 0.39967,
-        userIdt: 1,
-      },
-      {
-        cuota: 1781.30,
-        name: "Cronograma 2".toUpperCase(),
-        tem: 0.36908,
-        saldoInicial: 70000,
-        seguroDesgravamen: 0.028,
-        van: 16953.10,
-        tir: 0.245687,
-        userIdt: 2,
-      },
-      {
-        cuota: 1668.25,
-        name: "Cronograma 3".toUpperCase(),
-        tem: 0.42779,
-        saldoInicial: 1000,
-        seguroDesgravamen: 0.028,
-        van: 23973.30,
-        tir: 0.39967,
-        userIdt: 1,
-      },
-      {
-        cuota: 1781.30,
-        name: "Cronograma 4".toUpperCase(),
-        tem: 0.56908,
-        saldoInicial: 800,
-        seguroDesgravamen: 0.028,
-        van: 16953.10,
-        tir: 0.245687,
-        userIdt: 2,
-      }
-    ];
-
   }
 
   openDialog(element: Schedule): void {
